11a086efaa255d3abc3afca916769c6429ec016a
Jaime Perez <jaime.perezp00@gmail.com>
2024-03-19 19:47:29 -0300

    Ultimas modificaciones y Script

--- script/script.js
+++ script/script.js
@@ -1,43 +1,100 @@
-let vocales = ["a", "e", "i", "o", "u"];
-let claves = ["ai", "enter", "imes", "ober", "ufat"];
-let areatexto = document.getElementById ("input");
-let texto = areatexto.textContent;
+var botonEncriptar = document.querySelector(".btn-encriptar");
+var botonDesencriptar = document.querySelector(".btn-desencriptar");
+var munieco = document.querySelector(".contenedormunieco");
+var contenedor = document.querySelector(".contenedor-parrafo");
+var resultado = document.querySelector(".texto-resultado");
 
-console.log(texto);
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
 
-
-function encriptar(texto) {
-  let textoEncriptado = "";
-  for (let i = 0; i < texto.length; i++) {
-    const letra = texto[i];
-    const indiceVocal = vocales.indexOf(letra);
-    if (indiceVocal !== -1) {
-      textoEncriptado += claves[indiceVocal];
-    } else {
-      textoEncriptado += letra;
-    }
-  }
-  return textoEncriptado;
+function encriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = encriptarTexto(cajatexto);
 }
 
-function desencriptar(textoEncriptado) {
-  let textoDesencriptado = "";
-  for (let i = 0; i < textoEncriptado.length; i++) {
-    const caracter = textoEncriptado[i];
-    const indiceClave = claves.indexOf(caracter);
-    if (indiceClave !== -1) {
-      textoDesencriptado += vocales[indiceClave];
-    } else {
-      textoDesencriptado += caracter;
-    }
-  }
-  return textoDesencriptado;
+function desencriptar(){
+    ocultarAdelante();
+    var cajatexto = recuperarTexto()
+    resultado.textContent = desencriptarTexto(cajatexto);
 }
 
-// Ejemplo de uso
-const mensajeOriginal = "Hola ";
-const mensajeEncriptado = encriptar(mensajeOriginal);
-console.log("Mensaje encriptado:", mensajeEncriptado); // Salida: "H1l2 m3nd4!"
+function recuperarTexto(){
+    var cajatexto = document.querySelector(".cajatexto")
+    return cajatexto.value
+}
 
-const mensajeDesencriptado = desencriptar(mensajeEncriptado);
-console.log("Mensaje desencriptado:", mensajeDesencriptado); // Salida: "Hola mundo!"
+function ocultarAdelante(){
+    munieco.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+}
+
+function encriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
+
+}
+
+function desencriptarTexto(mensaje){
+    var texto = mensaje;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i];
+        }
+        
+    }
+
+    return textoFinal;
+
+}
+
+const btnCopiar = document.querySelector(".btn-copiar"); 
+    btnCopiar.addEventListener("click", copiar = () => {
+    var contenido = document.querySelector(".texto-resultado").textContent;
+    navigator.clipboard.writeText(contenido);
+});
